066a7353ad1134bb97c96ed66299f7bbbe29d271
Edgar Vidriales <edgarvidriales@gmail.com>
2025-09-01 17:52:19 -0700

    Initial Commit

---
+++ Exercise1/Exercise1App.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise1App.swift
+//  Exercise1
+//
+//  Created by Edgar Vidriales on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Exercise1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
